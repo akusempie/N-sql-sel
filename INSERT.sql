f4148e8a1aabeea50733e7cff8a34e48dc922697
akusempie <99676279+akusempie@users.noreply.github.com>
2022-10-31 22:54:45 +0200

    Add files via upload

---
+++ INSERT.sql
@@ -0,0 +1,23 @@
+INSERT INTO artists(name)
+	VALUES('Green Day'),
+	('Metallica'),
+	('30 Seconds to Mars'),
+	('Shpongle'),
+	('Lady Gaga'),
+	('LP'),
+	('Grimes'),
+	('Crystal Castles');
+	
+INSERT INTO genres(name)
+	VALUES ('Rock'),
+	('Pop'),
+	('Psytrance'),
+	('Electronic'),
+	('Metal');
+
+INSERT INTO albums(name, year)
+	VALUES('America', 2018),
+	('Chromatica', 2020),
+	
+	
+	
